20af4268a57be9b49ee6307161d41bab9e2e93f0
Grace Mullins <gmullins0728@gmail.com>
2020-06-04 13:51:28 -0400

    second commit

--- backend/api.js
+++ backend/api.js
@@ -1,14 +1,14 @@
 const https = require("https");
 
 // Google API_KEY
-googleApiKey = 'AIzaSyDKHFCMQiqFcNERDDcHqqcHRvNqZdqCLQU';
+// googleApiKey = 'AIzaSyDKHFCMQiqFcNERDDcHqqcHRvNqZdqCLQU';
 
 // Google Map API URL
-mapApiUrl = `https://maps.googleapis.com/maps/api/js?key=${this.googleApiKey}&callback=initMap`;
+// mapApiUrl = `https://maps.googleapis.com/maps/api/js?key=${this.googleApiKey}&callback=initMap`;
 
 // Google Translate API URL
-translateApiUrl = `https://translation.googleapis.com/$discovery/rest?version=v3`;
+// translateApiUrl = `https://translation.googleapis.com/$discovery/rest?version=v3`;
 
 // Eventful API KEY & API URL
-eventApiKey = 'S7N4hXRm85fgzH7G';
-eventApiUrl = 'http://api.eventful.com/rest/';
+// eventApiKey = 'S7N4hXRm85fgzH7G';
+// eventApiUrl = 'http://api.eventful.com/rest/';
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +9,23 @@ export class ApiService {
   // Google API_KEY
 googleApiKey = 'AIzaSyDKHFCMQiqFcNERDDcHqqcHRvNqZdqCLQU';
 
-// Google Map API URL
-mapApiUrl = `https://maps.googleapis.com/maps/api/js?key=${this.googleApiKey}&callback=initMap`;
-
-// Google Translate API URL
-translateApiUrl = `https://translation.googleapis.com/$discovery/rest?version=v3`;
-
 // Eventful API KEY & API URL
 eventApiKey = 'S7N4hXRm85fgzH7G';
-eventApiUrl = 'http://api.eventful.com/rest/';
 
   constructor(private http: HttpClient) { }
 
-  getCulture(): Observable
+  getLocation() {
+    const api = `https://maps.googleapis.com/maps/api/js?key=${this.googleApiKey}&callback=initMap`;
+    return this.http.get(api);
+}
+
+  getTranslation() {
+    const api = 'https://translation.googleapis.com/$discovery/rest?version=v3';
+    return this.http.get(api);
+  }
+
+  getEvent() {
+    const api = `http://api.eventful.com/json/events/search?app_key=${this.eventApiKey}`;
+    return this.http.get(api);
+  }
 }
