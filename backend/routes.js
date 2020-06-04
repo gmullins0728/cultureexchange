66a1597193dc725e2edeb0b1528094a68e00790a
michael <michaelsyfax@gmail.com>
2020-06-04 15:11:25 -0400

    Changes from setting up my project

--- backend/routes.js
+++ backend/routes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const api = require("./api");
 
 router.get("/api", (req, res) => {
-    console.log(req.body);
-    res.json(api);
+  console.log(req.body);
+  res.json(api);
 });
 
 module.exports = router;
