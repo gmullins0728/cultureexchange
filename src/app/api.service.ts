063dd2596dfc547014af3fa36a8b43415df5da94
Chelsea Wylie <chelsswylie@gmail.com>
2020-06-15 12:52:03 -0400

    with revised getHolidays API

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   // Calanderific API KEY
-  holidayApiKey = '49f2edd5bf043a727f73649144947c6fc5aa7ebb7c856909112ef7a91a0f00b3';
+  holidayApiKey =
+    '49f2edd5bf043a727f73649144947c6fc5aa7ebb7c856909112ef7a91a0f00b3';
   holidayBaseUrl = 'https://calendarific.com/api/v2/holidays';
-
-  
 
   constructor(private http: HttpClient) {}
 
@@ -20,12 +18,13 @@ export class ApiService {
   // }
 
   getHolidays() {
-    const api = `${this.holidayBaseUrl}?&api_key=${this.holidayApiKey}&country=US&year=2020`;
+    const api = `${this.holidayBaseUrl}?&api_key=${this.holidayApiKey}&country=US&month=7&year=2020`;
     return this.http.get(api);
   }
 
   getCulture() {
-    const api = 'https://data.opendatasoft.com/api/records/1.0/search/?dataset=world-heritage-list%40public-us&facet=category&facet=region&facet=states';
+    const api =
+      'https://data.opendatasoft.com/api/records/1.0/search/?dataset=world-heritage-list%40public-us&facet=category&facet=region&facet=states';
     // const api = 'https://data.opendatasoft.com/api/records/1.0/search/?dataset=world-heritage-list%40public-us&facet=category&facet=region&facet=states&refine.states=China'
     return this.http.get(api);
   }
