0656e2ee584ab10c6f39dc18fb83f9d0611c4d78
Chelsea Wylie <chelsswylie@gmail.com>
2020-06-15 12:47:06 -0400

    Holidays Component

--- src/app/holidays/holidays.component.ts
+++ src/app/holidays/holidays.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../api.service';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-holidays',
@@ -8,18 +9,18 @@ import { ApiService } from '../api.service';
   styleUrls: ['./holidays.component.css'],
 })
 export class HolidaysComponent implements OnInit {
-  holidays;
-  constructor(
-    private apiService: ApiService, 
-    public route: ActivatedRoute) {}
+  holidays: any;
+
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getHolidays().subscribe((data) => {
       this.holidays = data;
-      console.log(this.holidays);
       console.log(this.holidays.response.holidays);
-      console.log(this.holidays.response.holidays[0]?.name);
-      console.log(this.holidays.response.holidays[0].country.id);
+      console.log(this.holidays.response.holidays[0].name);
     });
   }
 }
+
+// https://calendarific.com/api/v2/holidays?&api_key=49f2edd5bf043a727f73649144947c6fc5aa7ebb7c856909112ef7a91a0f00b3&country=KR&year=2020
+// For above, get the country code for the countries that we have picked
