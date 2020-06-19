90eb0ad49e96a9c7d0f8be3a17a9ecc0621c9576
Michael Syfax <michaelsyfax@gmail.com>
2020-06-19 15:35:02 -0400

    clean up MAIN more and finished contact cards in COMMUNITY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ProfileComponent } from './profile/profile.component';
 import { CountriesComponent } from './countries/countries.component';
 import { HolidaysComponent } from './holidays/holidays.component';
 import { CultureComponent } from './culture/culture.component';
@@ -26,7 +25,6 @@ import { CommunityComponent } from './community/community.component';
     AppComponent,
     MainComponent,
     NavBarComponent,
-    ProfileComponent,
     CountriesComponent,
     HolidaysComponent,
     CultureComponent,
