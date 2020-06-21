7b543309a71268e6ad61d59721b86647d0aac615
Grace Mullins <gmullins0728@gmail.com>
2020-06-21 13:13:49 -0400

    Routing from Country to Culture done

--- src/app/holidays/holidays.component.ts
+++ src/app/holidays/holidays.component.ts
@@ -8,42 +8,37 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./holidays.component.css'],
 })
 export class HolidaysComponent implements OnInit {
-  holidays: any[] = [];
-  holidayCopy: any[] = [];
-  dayArray: string[] = [];
+  holidays: any[];
+  holidayCopy: any[];
+  dayArray: string[];
+  
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getHolidays().subscribe((data: any) => {
-      this.holidays = data.response.holidays;
-      this.holidayCopy = data.response.holidays;
-
-      this.dayArray = this.holidays.map((h) => h?.date?.iso);
-      // console.log('data', this.holidays);
+    this.holidays = data.response.holidays;
+    this.holidayCopy = data.response.holidays;
+    this.dayArray = this.holidays.map((h) => h?.date?.iso);
     });
   }
 
   myFunction(val): void {
-    //console.log('holidays', this.holidays);
-    console.log(val.key);
-    let results = this.holidays.filter((holiday) =>
-      holiday.country.name.includes(val.key)
+    console.log('holidays', this.holidays);
+    let results = this.holidays.filter((holiday) => val === holiday.country.name
     );
     if (results.length > 0) {
       this.holidays = results;
     } else {
       this.holidays = this.holidayCopy;
     }
-    console.log('results', results);
   }
 
   getDays(val): any {
     // console.log('days', new Date(this.holidays[0].date.iso).getTime());
-
-      let results = this.holidays.sort((d1, d2) => {
-      let dateA = new Date(d1?.date?.iso);
-      let dateB = new Date(d2?.date?.iso);
-      return dateA < dateB ? -1 : dateA > dateB ? 1 : 0;
+    let results = this.holidays.sort((d1, d2) =>  {
+    let dateA = new Date(d1?.date?.iso);
+    let dateB = new Date(d2?.date?.iso);
+    return dateA < dateB ? -1 : dateA > dateB ? 1 : 0;
     });
 
     if (results.length > 0) {
